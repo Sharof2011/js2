6c0775d8d022f6a19f61537773e4c529b1151184
Sharof2011 <shmaahrabbek@gmail.com>
2025-06-16 22:23:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// let - o'zgaruvchi, faqat lokal (mahalliy) scope da ishlaydi
+// var - eski usul, global scope ga ega bo'lishi mumkin
+// const - o'zgarmas qiymatli o'zgaruvchi
+
+// Misollar (kommentda):
+// let name = 'Sharof tok'
+// var username = 'ibrohim tentak 💩'
+// const studentname = 'kamrok eshak miya'
+
+// let surname = username = 'ibrohim yaxshi bola'
+// let sharofname = name = 'sharof gazanda'
+
+// Asosiy ish bajaruvchi kodlar:
+let username = prompt("Ismingizni kiriting"); // foydalanuvchidan ism so'raladi
+let rokname = prompt("Familiyangizni kiriting");
+let fuckname = prompt("Yoshingizni kiriting");
+let lolname = prompt("Yoqtirgan faningizni kiriting");
+let fullname = prompt("Yashaydigan tumaningizni kiriting");
+
+
+alert("Malumotlaringizni kiritganiz uchun raxmat! 😊");
